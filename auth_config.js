23579a579c6022fd5231a555dba2590f3b418a21
pjesek <fabisiak.tomek@gmail.com>
2016-08-23 12:46:19 +0100

    Deletion

--- auth_config.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Pjesek on 20.07.2016.
- */
-module.exports = {
-    jwtSecret: "MyS3cr3tK3Y",
-    jwtSession: {session: false}
-};
--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-
-    'secret': 'ilovescotchyscotch',
-    'database': 'mongodb://admin:password@172.17.0.2/myapp'
-
-};
